161d13f60f3424f2d1489420eca7afe0d051e894
Robert Jördens <rj@quartiq.de>
2024-09-25 15:02:26 +0200

    cover more, document

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,48 @@
 #![no_std]
+#![deny(rust_2018_compatibility)]
+#![deny(rust_2018_idioms)]
+#![warn(missing_docs)]
+#![forbid(unsafe_code)]
+
+//! Provide `core::error::Error` for `embedded-hal` Errors using a newtype wrapper.
 
 use core::error;
 use core::fmt::{Debug, Display};
+use core::ops::{Deref, DerefMut};
 
+/// Wrap an Error and store its ErrorKind
+///
+/// This implements [`core::error::Error`] by using the
+/// `E: Debug` for `Debug` and `Display` and the
+/// `E: ErrorKind` as [`core::error::Error::source()`].
 pub struct Error<E, K> {
     inner: E,
     kind: K,
 }
 
+impl<E, K> Error<E, K> {
+    /// Extract the inner Error
+    pub fn into_inner(self) -> E {
+        self.inner
+    }
+}
+
 impl<E: Debug, K> Display for Error<E, K> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         Debug::fmt(&self.inner, f)
+    }
+}
+
+impl<E, K> Deref for Error<E, K> {
+    type Target = E;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<E, K> DerefMut for Error<E, K> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
     }
 }
 
@@ -43,6 +75,7 @@ impl_from!(embedded_hal::pwm);
 impl_from!(embedded_hal::spi);
 impl_from!(embedded_can);
 impl_from!(embedded_hal_nb::serial);
+impl_from!(embedded_io);
 
 #[cfg(test)]
 mod tests {
@@ -80,15 +113,16 @@ mod tests {
         #[error("logic")]
         Logic,
     }
-    fn driver<P: digital::OutputPin>(p: &mut P) -> Result<(), DriverError<P::Error>> {
-        p.set_high().map_err(Error::from)?;
-        if p.set_low().is_err() {
+    fn driver<P: digital::OutputPin>(pin: &mut P) -> Result<(), DriverError<P::Error>> {
+        pin.set_high().map_err(Error::from)?;
+        if pin.set_low().is_err() {
             Err(DriverError::Logic)
         } else {
             Ok(())
         }
     }
 
+    // user
     #[test]
     fn it_works() {
         let driver_err: DriverError<PinError> = driver(&mut Pin).unwrap_err();
